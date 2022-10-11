a5a9ee0dd86fba68eca5a85e8542839601ccd03a
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-10-11 21:03:47 +0600

    revamped with context API

--- src/components/Topics/Topic/Topic.jsx
+++ src/components/Topics/Topic/Topic.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { TopicContext } from "../Topics";
 
-const Topic = props => {
-  const { id, name, logo, total } = props.topic;
+const Topic = () => {
+  const topic = useContext(TopicContext);
+  const { id, name, logo, total } = topic;
   return (
     <article className="bg-slate-300 p-6 flex flex-col gap-5">
-      <img src={logo} alt={name} />
+      <img
+        src={logo}
+        alt={name}
+        loading="lazy"
+        width="312"
+        height="312"
+        decoding="async"
+        fetchpriority="low"
+      />
       <div className="flex flex-wrap gap-4 justify-between items-center">
         <h3 className="text-4xl break-all">{name}</h3>
-        <span className="">quiz: {total}</span>
+        <span className="">questions: {total}</span>
       </div>
       <Link
         to={`../quiz/${id}`}
         type="button"
-        className="w-full p-4 bg-blue-600 text-white hover:bg-slate-600 transition">
-        take a test
+        className="w-full p-4 text-center font-bold text-xl bg-blue-600 text-white hover:bg-slate-600 transition">
+        Take a test
       </Link>
     </article>
   );
--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { createContext, useContext } from "react";
 import { TopicsContext } from "../../pages/Main/Main";
 import Topic from "./Topic/Topic";
+export const TopicContext = createContext({});
 
 const Topics = () => {
   const topics = useContext(TopicsContext);
@@ -8,11 +9,15 @@ const Topics = () => {
     <section className="px-4 pt-8 lg:pt-20 min-h-screen flex flex-col justify-center">
       <div className="container mx-auto py-24">
         <h2 className="text-6xl text-center pb-10 lg:pb-20">
-          Take any quiz you want
+          Take any quiz you want out of{" "}
+          <b className="text-blue-600">{topics.length}</b> quizzes
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {/* passing dynamic data to topic */}
           {topics.map(topic => (
-            <Topic key={topic.id} topic={topic}></Topic>
+            <TopicContext.Provider key={topic.id} value={topic}>
+              <Topic />
+            </TopicContext.Provider>
           ))}
         </div>
       </div>
