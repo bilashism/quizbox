a039b63524eaeb412f434ab2dace2a4a767b519a
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-10-11 10:39:41 +0600

    revamped routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Router";
 function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Header></Header>
-      <Footer></Footer>
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, Route } from "react-router-dom";
-import { router } from "./routes/Router";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App></App>
   </React.StrictMode>
 );
