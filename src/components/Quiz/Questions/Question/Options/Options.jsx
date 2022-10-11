f5cf18a4ca8dfd1aa8e29f5234267e7af2ea9937
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-10-11 21:46:52 +0600

    updated radio button

--- src/components/Quiz/Questions/Question/Options/Options.jsx
+++ src/components/Quiz/Questions/Question/Options/Options.jsx
@@ -26,6 +26,7 @@ const Options = () => {
           <input
             type="radio"
             name={`option-${id}`}
+            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
             onChange={option === correctAnswer ? rightAnswer : wrongAnswer}
           />
           {option}
